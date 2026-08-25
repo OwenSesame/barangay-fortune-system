c8f57d6ae77ebaad7b7b4e30f5b0f2b62376f053
Owen Hart De Dios <dediosowen@gmail.com>
2026-08-25 17:40:04 +0800

    Fix

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,6 +20,7 @@ export default function Login() {
       const userId = response.data.id;
 
       // Store basic auth info
+      localStorage.setItem('token', response.data.token);
       localStorage.setItem('userId', userId);
       localStorage.setItem('userRole', userRole);
 
